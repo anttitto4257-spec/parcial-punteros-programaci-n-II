b62ef1684d7f0e66b8cab695f7e2a33d6aee7d11
antonella.cordoba <anttitto4257@gmail.com>
2025-10-28 20:08:09 -0300

    punteros
    Removed unnecessary includes and simplified the main function. The matrix size input and random generation logic were also removed.

--- tp 1 ejer 3 suma de matrices.c
+++ tp 1 ejer 3 suma de matrices.c
@@ -1,70 +1,10 @@
-#include <stdio.h> //scanf y printf 
-#include <stdlib.h>// para rand() y srand()
-#include <time.h> // para time ()  
+#include <stdio.h>
 
-int main () {
+int main() {
+    int x = 10;       // Variable normal
+    int *p;           // Declaramos un puntero a entero
 
-	int N;
-
-	//pedir tamaC1o de la matriz
-	printf ("Ingresar tamaC1o de la matriz : ");
-	scanf("%d", &N);
-
-	// Declaramos las matrices [A][B] y suma
-	int A[N][N];
-	int B[N][N];
-	int suma[N][N];
-
-	// iniciamos el generador de  numeros aleatorios
-	srand(time(NULL));
-
-	//generar valores aleatorios entre 1 y 9 para ambas matrices
-	for (int f = 0 ; f < N ; f++) {
-		for (int c = 0; c < N ; c++) {
-			A[f][c]=rand() % 9 + 1;
-			B[f][c]=rand() % 9 + 1;
-		}
-	}
-
-	// mostrar matriz 1
-	printf("\nContenido de las matrices\n");
-	printf("Matriz 1\n");
-	for (int f = 0; f < N ; f++) {
-		for (int c = 0; c < N ; c++) {
-			printf("%d ", A[f][c]);
-		}
-		printf("\n");
-	}
-
-	//mostrar matriz 2
-	printf("matriz 2\n");
-	for ( int f = 0; f < N ; f++) {
-		for ( int c = 0; c < N ; c++) {
-		    printf("%d ", B[f][c]);
-}
-	printf("\n");
-	}
-	
-	
-	// calcular la suma de ambas matrices 
-	for (int f = 0 ; f < N ; f++) {
-	    for (int c = 0; c < N ; c++){
-	      suma[f][c]= A[f][c] + B[f][c];
-	    }
-	     
-	}
-	 
-	 //mostrar la suma
-	 printf("Suma de las matrices\n");
-	 for ( int f = 0; f < N ; f++) {
-	     for (int c = 0; c < N ; c++) {
-	         printf("%d ",suma[f][c]);
-	     }
-	     printf("\n");
-	 }
-	return 0;
-}
-
+    p = &x;           // Asignamos a p la dirección de memoria de x
 
 
 
