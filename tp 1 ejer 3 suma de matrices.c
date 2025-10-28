6f852c58fd83aa51c643e83aa871ce8469254671
antonella.cordoba <anttitto4257@gmail.com>
2025-10-28 20:09:14 -0300

    Correct pointer value assignment and comments
    Fix pointer assignment and comment formatting.

--- tp 1 ejer 3 suma de matrices.c
+++ tp 1 ejer 3 suma de matrices.c
@@ -5,6 +5,8 @@ int main() {
     int *p;           // Declaramos un puntero a entero
 
     p = &x;           // Asignamos a p la dirección de memoria de x
+*p = 20 //cambia el valor en x y por lo tanto cMBIA EL VALOR DENTRO DEL PUNTERO PERO  no la direccion donde apunta
+    
 
 
 
